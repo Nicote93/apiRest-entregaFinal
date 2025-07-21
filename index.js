70dd8bb05cab4bbdf2d209f04a232f000177e5bb
Nicote93 <nicolas.vite.1993@gmail.com>
2025-07-21 19:03:36 -0300

    actualizacion en PORT

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 import productsRouter from "./src/routes/products.router.js";
 app.use("/api", productsRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
